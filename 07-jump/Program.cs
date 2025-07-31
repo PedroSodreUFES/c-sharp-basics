cb85b899a1712bacb9060aca76fe88694ef0b7c9
PedroSodreUFES <pedro.malini@edu.ufes.br>
2025-07-31 02:37:33 -0300

    feat: jump commands - break, continue e return

---
+++ 07-jump/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace HelloWorld;
+
+public class Program
+{
+    public static void Main()
+    {
+        var numero = 0;
+
+        // break
+        Console.WriteLine("break:");
+        while (numero < 10)
+        {
+            numero++;
+            if (numero == 5)
+            {
+                break;
+            }
+            Console.WriteLine(numero);
+        }
+        Console.WriteLine("Aqui!");
+
+        numero = 0;
+        // continue
+        Console.WriteLine("\ncontinue:");
+        while (numero < 10)
+        {
+            numero++;
+            if (numero == 5)
+            {
+                continue;
+            }
+            Console.WriteLine(numero);
+        }
+        Console.WriteLine("Aqui!");
+
+
+        numero = 0;
+        // return => quebra a função
+        Console.WriteLine("\nreturn:");
+        while (numero < 10)
+        {
+            numero++;
+            if (numero == 5)
+            {
+                return; // saiu da função
+            }
+            Console.WriteLine(numero);
+        }
+        Console.WriteLine("Aqui!"); // não será printado
+    }
+}
